b1a6b111b832220cc6745955c7ed7892a7678aea
Tonaxe <ymennana@elpuig.xeill.net>
2024-03-05 16:19:54 +0100

    sisuss

--- app/src/main/java/com/example/judas/HomeFragment.java
+++ app/src/main/java/com/example/judas/HomeFragment.java
@@ -5,8 +5,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private ImageAdapter imageAdapter;
+    private List<Integer> imageList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +27,21 @@ public class HomeFragment extends Fragment {
         // Inflar el diseño del fragmento
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Retornar la vista inflada
+        imageList = new ArrayList<>();
+        imageList.add(R.drawable.uno);
+        imageList.add(R.drawable.dos);
+        imageList.add(R.drawable.tres);
+        imageList.add(R.drawable.quatro);
+        imageList.add(R.drawable.cinco);
+        imageList.add(R.drawable.seis);
+        // Agrega las demás imágenes...
+
+        // Inicializar RecyclerView
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        imageAdapter = new ImageAdapter(getContext(), imageList);
+        recyclerView.setAdapter(imageAdapter);
+
         return rootView;
     }
 }
