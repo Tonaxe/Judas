672b69aaec752c7c6c30971404c800fe438fd37d
Tonaxe <ymennana@elpuig.xeill.net>
2024-03-05 22:19:28 +0100

    ci

---
+++ app/src/main/java/com/example/judas/ResetContraQuatroFragment.java
@@ -0,0 +1,42 @@
+package com.example.judas;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ResetContraQuatroFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reset_contra_quatro, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Obtener referencia a la imagen
+        View btnIniciaSesin = view.findViewById(R.id.btnIniciaSesin);
+
+        // Configurar el clic de la imagen para navegar al fragmento LogRegInicioFragment
+        btnIniciaSesin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener NavController desde el Fragment
+                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+
+                // Navegar al fragmento LogRegInicioFragment utilizando la acción definida en nav_graph.xml
+                navController.navigate(R.id.action_resetContraQuatroFragment_to_logInFragment);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/judas/ResetContraTresFragment.java
+++ app/src/main/java/com/example/judas/ResetContraTresFragment.java
@@ -53,5 +53,20 @@ public class ResetContraTresFragment extends Fragment {
                 navController.navigate(R.id.action_resetContraTresFragment_to_logInFragment);
             }
         });
+
+        // Obtener referencia a la imagen
+        View btnRestaurarLaContrasea = view.findViewById(R.id.btnRestaurarLaContrasea);
+
+        // Configurar el clic de la imagen para navegar al fragmento LogRegInicioFragment
+        btnRestaurarLaContrasea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener NavController desde el Fragment
+                NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+
+                // Navegar al fragmento LogRegInicioFragment utilizando la acción definida en nav_graph.xml
+                navController.navigate(R.id.action_resetContraTresFragment_to_resetContraQuatroFragment);
+            }
+        });
     }
 }
